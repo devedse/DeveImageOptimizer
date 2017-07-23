ccd39d7feb1e8fe0c9ca919b21d117e7cb1836eb
Devedse <davydavidse@gmail.com>
2017-07-23 03:10:15 +0200

    Added VIPS nuget package

--- DeveImageOptimizer/Constants.cs
+++ DeveImageOptimizer/Constants.cs
@@ -5,6 +5,7 @@
         public static string[] ValidExtensions { get; } = new string[] { ".PNG", ".JPG", ".JPEG", ".GIF", ".BMP" };
         public const string TempDirectoryName = "Temp";
 
-        public const string VipsDir = @"C:\Users\Davy\Downloads\VIPS";
+        //public const string VipsDir = @"C:\Users\Davy\Downloads\VIPS";
+        public const string VipsDir = @".nuget\packages\vipsnuget\1.0.2\content\VIPS";
     }
 }
--- DeveImageOptimizer/ImageConversion/ImageConverter.cs
+++ DeveImageOptimizer/ImageConversion/ImageConverter.cs
@@ -13,7 +13,9 @@ namespace DeveImageOptimizer.ImageConversion
         {
             Directory.CreateDirectory(FolderHelperMethods.TempDirectoryForTests.Value);
 
-            string pathVips = Path.Combine(Constants.VipsDir, "vips.exe");
+            var userProfileDir = System.Environment.GetEnvironmentVariable("USERPROFILE");
+            //var userProfileDir2 = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string pathVips = Path.Combine(userProfileDir, Constants.VipsDir, "vips.exe");
 
             var imageName = Path.GetFileNameWithoutExtension(inputPath);
             var outPath = Path.Combine(FolderHelperMethods.TempDirectoryForTests.Value, $"{imageName}.png");
