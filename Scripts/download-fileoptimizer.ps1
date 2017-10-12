4b824f5fcf508bcd5d76674753ee2b90853592ed
Devedse <davydavidse@gmail.com>
2017-10-12 16:28:40 +0200

    Improved script

--- Scripts/download-fileoptimizer.ps1
+++ Scripts/download-fileoptimizer.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
 $url = 'https://sourceforge.net/projects/nikkhokkho/files/FileOptimizer/11.10.2015/FileOptimizerFull.7z.exe'
-$path = '.\FileOptimizerFull.7z.exe'
+$path = Join-Path $scriptPath 'FileOptimizerFull.7z.exe'
 
 Write-Host "Downloading file..."
 #Invoke-WebRequest -Uri $url -OutFile $path -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox
