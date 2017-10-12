8edcd814e8bbd56ea5438133aab01e7bef7d2e78
Devedse <davydavidse@gmail.com>
2017-10-12 16:25:04 +0200

    Fixed script

--- Scripts/download-fileoptimizer.ps1
+++ Scripts/download-fileoptimizer.ps1
@@ -4,13 +4,22 @@ $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $url = 'https://sourceforge.net/projects/nikkhokkho/files/FileOptimizer/11.10.2015/FileOptimizerFull.7z.exe'
 $path = '.\FileOptimizerFull.7z.exe'
 
-Invoke-WebRequest -Uri $url -OutFile $path -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox
+Write-Host "Downloading file..."
+#Invoke-WebRequest -Uri $url -OutFile $path -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox
+$wc = New-Object net.webclient
+$wc.Downloadfile($url, $path)
 
-$p = Start-Process $path '-o".\FileOptimizer" -y' -NoNewWindow -Wait
+Write-Host "File downloaded, extracting..."
+$p = Start-Process $path '-o".\FileOptimizer" -y' -Wait -Passthru
 $p.WaitForExit()
 if ($p.ExitCode -ne 0) {
+	Write-Host "Extraction failed, exiting."
     $host.SetShouldExit($p.ExitCode)
 }
 
+Write-Host "Extraction completed, copying ini file."
+
 $iniFile = Join-Path $scriptPath 'FileOptimizer64.ini'
 cp $iniFile .\FileOptimizer\FileOptimizer64.ini
+
+Write-Host "Script completed"
