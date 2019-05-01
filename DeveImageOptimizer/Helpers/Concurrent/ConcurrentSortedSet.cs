4464ace81d347431d51d8ca025e6d81a90193d02
Devedse <davydavidse@gmail.com>
2019-05-01 16:45:49 +0200

    Added concurrent hash set

---
+++ DeveImageOptimizer/Helpers/Concurrent/ConcurrentSortedSet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeveImageOptimizer.Helpers.Concurrent
+{
+    public class ConcurrentSortedSet<T> : IEnumerable<T>
+    {
+        private readonly SortedSet<T> internalSet;
+        private readonly object lockject = new object();
+
+        public ConcurrentSortedSet()
+        {
+            internalSet = new SortedSet<T>();
+        }
+
+        public ConcurrentSortedSet(IEnumerable<T> values)
+        {
+            internalSet = new SortedSet<T>(values);
+        }
+
+        public bool Add(T value)
+        {
+            return TryAdd(value);
+        }
+
+        public bool TryAdd(T value)
+        {
+            lock (lockject)
+            {
+                return internalSet.Add(value);
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            lock (lockject)
+            {
+                return internalSet.Contains(value);
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            lock (lockject)
+            {
+                return internalSet.ToList().GetEnumerator();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            lock (lockject)
+            {
+                return internalSet.ToList().GetEnumerator();
+            }
+        }
+    }
+}
