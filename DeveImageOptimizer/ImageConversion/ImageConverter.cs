b7ea794405acc95afbdfaa7a047ec1fcf16b6e4e
Devedse <davydavidse@gmail.com>
2020-08-04 22:22:40 +0200

    Removed libvips dependency

--- DeveImageOptimizer/ImageConversion/ImageConverter.cs
+++ DeveImageOptimizer/ImageConversion/ImageConverter.cs
@@ -1,6 +1,4 @@
-﻿using DeveImageOptimizer.Helpers;
-using System.Diagnostics;
-using System.IO;
+﻿using System;
 
 namespace DeveImageOptimizer.ImageConversion
 {
@@ -8,24 +6,25 @@ namespace DeveImageOptimizer.ImageConversion
     {
         public static string ConvertJpgToPngWithAutoRotate(string inputPath)
         {
-            string pathVips = DeveLibVipsNuget.LibVipsManager.ExtractAndGetVipsExeFile();
+            throw new InvalidOperationException("Using VIPS has been removed from DeveImageOptimizer because ImageSharp works great now and no bugs have to be worked around");
+            //string pathVips = DeveLibVipsNuget.LibVipsManager.ExtractAndGetVipsExeFile();
 
-            var imageName = Path.GetFileNameWithoutExtension(inputPath);
-            var outPath = Path.Combine(FolderHelperMethods.TempDirectory, RandomFileNameHelper.RandomizeFileName(imageName, "png"));
-            string args = $"COPY \"{inputPath}\"[autorotate] \"{outPath}\"";
+            //var imageName = Path.GetFileNameWithoutExtension(inputPath);
+            //var outPath = Path.Combine(FolderHelperMethods.TempDirectory, RandomFileNameHelper.RandomizeFileName(imageName, "png"));
+            //string args = $"COPY \"{inputPath}\"[autorotate] \"{outPath}\"";
 
-            var processStartInfo = new ProcessStartInfo(pathVips, args)
-            {
-                WindowStyle = ProcessWindowStyle.Hidden,
-                UseShellExecute = true,
-                CreateNoWindow = false
-            };
+            //var processStartInfo = new ProcessStartInfo(pathVips, args)
+            //{
+            //    WindowStyle = ProcessWindowStyle.Hidden,
+            //    UseShellExecute = true,
+            //    CreateNoWindow = false
+            //};
 
-            using (var proc = Process.Start(processStartInfo))
-            {
-                proc.WaitForExit();
-                return outPath;
-            }
+            //using (var proc = Process.Start(processStartInfo))
+            //{
+            //    proc.WaitForExit();
+            //    return outPath;
+            //}
         }
     }
 }
