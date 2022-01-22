6d3d311466e521818edb1031a74df2f50d942112
Devedse <davydavidse@gmail.com>
2022-01-22 23:57:59 +0100

    Updated exception message

--- DeveImageOptimizer/Exceptions/FileOptimizerNotFoundException.cs
+++ DeveImageOptimizer/Exceptions/FileOptimizerNotFoundException.cs
@@ -4,7 +4,8 @@ namespace DeveImageOptimizer.Exceptions
 {
     public class FileOptimizerNotFoundException : Exception
     {
-        public FileOptimizerNotFoundException(string? message) : base(message)
+        public FileOptimizerNotFoundException(string fileOptimizerPath) :
+            base($"Could not find FileOptimizer.exe in: '{fileOptimizerPath}'{Environment.NewLine}{Environment.NewLine}Please ensure FileOptimizer is installed before optimizing images. It can be downloaded here:{Environment.NewLine}https://sourceforge.net/projects/nikkhokkho/files/FileOptimizer/{Environment.NewLine}Or installed through Chocolatey:{Environment.NewLine}https://chocolatey.org/packages/FileOptimizer")
         {
         }
     }
